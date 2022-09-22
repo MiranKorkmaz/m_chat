8e15f49b0f0aeeb92fba69d3f859c0770218355d
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-09-22 20:42:32 +0200

    Add boilerplate for backend and frontend

---
+++ server/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, json, Request, Response } from "express";
+import cors from "cors";
+import { PostProps } from "./api/interfaces";
+
+const app: Application = express();
+app.use(cors()); // Configure CORS properly to secure app
+app.use(json());
+const port: number | string = process.env.PORT || 3001;
+
+app.get('/', (req: Request, res: Response<PostProps>) => {
+  res.send({
+    text: "HI!!!!"
+  })
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+});
