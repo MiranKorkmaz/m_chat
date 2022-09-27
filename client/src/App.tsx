2af4e16064dd2ff52db4c74b6ad0ea82c117ecd8
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-09-27 14:24:07 +0200

    Add eslint for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
 import './App.css';
-import { useEffect, useState } from "react";
-import { PostProps } from "./api/interfaces";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { PostProps } from './api/interfaces';
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = 'http://localhost:3001';
 
 const fetchPosts = async (): Promise<PostProps> => {
-  const response = await axios.get<PostProps>("/")
-  return response.data
+  const response = await axios.get<PostProps>('/');
+  return response.data;
 };
 
-const App = () => {
+function App() {
   const [post, setPost] = useState<PostProps | undefined>();
-  
   useEffect(() => {
-    fetchPosts().then(setPost)
+    fetchPosts().then(setPost);
   }, []);
-  
   return (
     <div className="App">
       <header className="App-header">
-        {post ? post.text : "No posts"}
+        {post ? post.text : 'No posts'}
       </header>
     </div>
   );
