51de6c54d03c00bfab25ef6defff247b575b19b5
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-09-29 12:04:37 +0200

    Add env variables to urls

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,16 @@
 import express, { Application, json } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import { mongoDbSetUp } from "./models/post-model";
 import postController from "./controllers/post-controller";
+
+dotenv.config();
 
 const app: Application = express();
 app.use(cors()); // Configure CORS properly to secure app
 app.use(json());
-const port: number | string = process.env.PORT || 3001;
-const mongoDbURL = "mongodb://localhost:27017/chat-app"
+const port: number | string = process.env.SERVER_PORT || 3001;
+const mongoDbURL: string = process.env.MONGO_URL || "4000";
 
 app.use('/posts', postController)
 
