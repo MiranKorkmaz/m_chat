af25b7454d1fe9705b16c4f76fd9a3fa17db0d23
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-09-29 15:31:18 +0200

    Add states for user and post

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { PostProps } from './api/interfaces';
+import { IPost } from './api/interfaces';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = process.env.REACT_APP_POST_API || 'http://localhost:3001';
 
-const fetchPosts = async (): Promise<PostProps> => {
-  const response = await axios.get<PostProps>('/');
+// fetch posts 
+const fetchPosts = async (): Promise<Array<IPost>> => {
+  const response = await axios.get<Array<IPost>>('/posts');
   return response.data;
 };
 
 function App() {
-  const [post, setPost] = useState<PostProps | undefined>();
+  const [post, setPost] = useState<Array<IPost>>([]);
+  const [error, setError] = useState<string | undefined>();
+  const [user, setUser] = useState<Array<IPost>>([]);
+  const [postText, setPostText] = useState<string>("");
+  
+  // create posts
 
   useEffect(() => {
     fetchPosts().then(setPost);
@@ -19,6 +25,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {}
         <p>{post ? post.text : 'No posts'}</p>
         <p>{post ? `${post.timeStamp.toString().split('T')[0]} at ${post.timeStamp.toString().split('T')[1].substring(0, 8)} ${parseInt(post.timeStamp.toString().split('T')[1].substring(0, 8), 10) >= 12 ? 'PM' : 'AM'} ` : 'No date'}</p>
       </header>
