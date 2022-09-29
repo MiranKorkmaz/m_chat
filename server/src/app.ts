cfc87834d178e74c68a207fa99e2708a735b6b5f
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-09-29 11:43:30 +0200

    Add basis for database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application, json, Request, Response } from "express";
+import express, { Application, json } from "express";
 import cors from "cors";
-import { IPost } from "./api/interfaces";
 import { mongoDbSetUp } from "./models/post-model";
+import postController from "./controllers/post-controller";
 
 const app: Application = express();
 app.use(cors()); // Configure CORS properly to secure app
@@ -9,12 +9,7 @@ app.use(json());
 const port: number | string = process.env.PORT || 3001;
 const mongoDbURL = "mongodb://localhost:27017/chat-app"
 
-app.get('/', (req: Request, res: Response<IPost>) => {
-  res.send({
-    text: "HI!!!!",
-    timeStamp: new Date()
-  })
-});
+app.use('/posts', postController)
 
 app.listen(port, async function () {
   await mongoDbSetUp(mongoDbURL)
