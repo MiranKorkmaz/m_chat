b3907c787fee53fe92c97917a06393edc2a7ff16
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-09-29 16:01:48 +0200

    Add input field and map over result

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { IPost } from './api/interfaces';
 
 axios.defaults.baseURL = process.env.REACT_APP_POST_API || 'http://localhost:3001';
 
-// fetch posts 
+// fetch posts
 const fetchPosts = async (): Promise<Array<IPost>> => {
   const response = await axios.get<Array<IPost>>('/posts');
   return response.data;
@@ -14,21 +14,66 @@ const fetchPosts = async (): Promise<Array<IPost>> => {
 function App() {
   const [post, setPost] = useState<Array<IPost>>([]);
   const [error, setError] = useState<string | undefined>();
-  const [user, setUser] = useState<Array<IPost>>([]);
-  const [postText, setPostText] = useState<string>("");
-  
+  const [postText, setPostText] = useState<string>('');
+  const [userNameText, setUserNameText] = useState<string>('');
+
   // create posts
+  const createPost = async (postText: string, userNameText: string): Promise<void> => {
+    const post: IPost = {
+      userName: userNameText,
+      text: postText,
+      timeStamp: new Date(),
+    };
+    try {
+      await axios.post<Array<IPost>>('/posts', post);
+      const response = await axios.get<Array<IPost>>('/posts');
+      setPost(response.data);
+    } catch (err) {
+      setPost([]);
+      setError('Something went wrong with fetching posts..');
+    }
+  };
 
   useEffect(() => {
-    fetchPosts().then(setPost);
+    fetchPosts().then(setPost).catch(() => {
+      setPost([]);
+      setError('Something went wrong with fetching posts');
+    });
   }, []);
   return (
     <div className="App">
       <header className="App-header">
-        {}
-        <p>{post ? post.text : 'No posts'}</p>
-        <p>{post ? `${post.timeStamp.toString().split('T')[0]} at ${post.timeStamp.toString().split('T')[1].substring(0, 8)} ${parseInt(post.timeStamp.toString().split('T')[1].substring(0, 8), 10) >= 12 ? 'PM' : 'AM'} ` : 'No date'}</p>
+        {post ? post.map((p) => (
+          <div key={p.id}>
+            <p key={p.id}>
+              Message:
+              {' '}
+              {p.text}
+            </p>
+            <p key={p.id}>
+              User:
+              {' '}
+              {p.userName}
+            </p>
+            <p key={p.id}>
+              Date:
+              {' '}
+              {p.timeStamp.toString().split('T')[0]}
+              {' '}
+              at
+              {' '}
+              {p.timeStamp.toString().split('T')[1].substring(0, 8)}
+              {' '}
+              {parseInt(p.timeStamp.toString().split('T')[1].substring(0, 8), 10) >= 12 ? 'PM' : 'AM'}
+            </p>
+          </div>
+        )) : error || 'Waiting for posts..'}
       </header>
+      <section>
+        <input type="text" value={postText} onChange={(e) => setPostText(e.target.value)} />
+        <input type="text" value={userNameText} onChange={(e) => setUserNameText(e.target.value)} />
+        <button type="submit" onClick={() => createPost(postText, userNameText)}>Send</button>
+      </section>
     </div>
   );
 }
