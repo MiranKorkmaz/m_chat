ada3792fd3459f75e2700dacb37500ac34d0e364
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-10-05 13:17:36 +0200

    Add error handling for username

--- server/src/services/post-service.ts
+++ server/src/services/post-service.ts
@@ -2,8 +2,8 @@ import { IPost } from "../api/interfaces";
 import { loadAllPosts, savePostToDB } from "../models/post-model";
 
 export const savePost = async (post: IPost): Promise<Array<IPost>> => {
-  if (!post.text || post.text === "") {
-    throw new Error ("Invalid text")
+  if (!post.text || post.text === "" || post.userName === "") {
+    throw new Error ("Invalid text or username")
   }
   post.timeStamp = new Date()
   await savePostToDB(post)
