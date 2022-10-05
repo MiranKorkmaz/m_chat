f25ae81afd45b2ef14549e091a50bf6888a849b5
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-10-05 13:00:59 +0200

    Let same user post twice

--- server/src/services/post-service.ts
+++ server/src/services/post-service.ts
@@ -2,8 +2,8 @@ import { IPost } from "../api/interfaces";
 import { loadAllPosts, savePostToDB } from "../models/post-model";
 
 export const savePost = async (post: IPost): Promise<Array<IPost>> => {
-  if (!post.text || post.text == "" || !post.userName || post.userName == "") {
-    throw new Error ("Invalid text or username")
+  if (!post.text || post.text === "") {
+    throw new Error ("Invalid text")
   }
   post.timeStamp = new Date()
   await savePostToDB(post)
